389b8ecb522abdf7b5c8b0243aa0c4375a170c2b
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-06-30 18:30:49 -0500

    Addloop in index

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php get_header(); ?>
 
-<h1>Hello World!</h1>
-<p>WordPress Site</p>
-
+<?php
+if(have_posts()){
+  while(have_posts()){
+    the_post();?>
+    <div>
+      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+      <?php the_post_thumbnail('medium'); ?>
+      <p><?php echo "Published: " . get_the_date(); ?></p>
+      <p><?php echo "Written by " . get_the_author(); ?></p>
+      <p><?php the_excerpt(); ?></p>
+    </div>
+<?php }
+}
+?>
 
 <?php get_footer(); ?>
