5c4ce3584abdae12e8f743a9b283a35d67494fc7
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-06-30 18:43:38 -0500

    Adjust the single post page

--- single.php
+++ single.php
@@ -8,7 +8,6 @@
       <?php the_post_thumbnail(); ?>
       <h2 class="post-title"><?php the_title(); ?></h2>
       <p><?php echo "Published: " . get_the_date(); ?></p>
-      <p><?php echo "This post is written by : " . get_the_author(); ?></p>
       <?php the_content(); ?>
       <?php
     }
