5ad1915c483852ab1cd86ea65fb0d2400f25d682
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-06-30 20:20:33 -0500

    Center index objects

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
         while(have_posts()){
           the_post();?>
           <div class="one-half column posts">
-            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-            <?php the_post_thumbnail('medium'); ?>
+            <h3 class="center-text"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <div class="img-center"><?php the_post_thumbnail('medium'); ?></div>
             <p><?php echo "Published: " . get_the_date(); ?></p>
             <p><?php echo "Written by " . get_the_author(); ?></p>
             <p><?php the_excerpt(); ?></p>
