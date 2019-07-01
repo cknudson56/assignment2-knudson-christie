b4dcb31677fb4e3cb7dccfcb2bae7f0483d6c471
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-06-30 20:43:20 -0500

    Add style to feature img on single page

--- single.php
+++ single.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 
-
+<div class="container">
 <?php
   if(have_posts()){
     while(have_posts()){
       the_post(); ?>
-      <?php the_post_thumbnail(); ?>
+      <div class="post-feature">
+        <?php the_post_thumbnail(); ?>
+      </div>
       <h2 class="post-title"><?php the_title(); ?></h2>
       <p><?php echo "Published: " . get_the_date(); ?></p>
       <?php the_content(); ?>
@@ -13,6 +15,7 @@
     }
   }
 ?>
+</div>
 
 
 <?php get_footer(); ?>
